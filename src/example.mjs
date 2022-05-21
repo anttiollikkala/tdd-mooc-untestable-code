c0fe9e21f17cc28fa3f3ad3b6b3a6c2f9c798abc
Antti Ollikkala <antti@huom.io>
2022-05-21 17:26:06 +0300

    untestable code

--- src/example.mjs
+++ src/example.mjs
@@ -1,3 +1,25 @@
-export function sum(a, b) {
-  return a + b;
+import net from 'net'
+import fs from'fs';
+
+let fileName = "./README.md"
+
+export const setFileName = (to) => {
+  fileName = to
+}
+
+let server = net.createServer(function(c) {
+  read_file_and_send_to_socket(c)
+});
+
+console.log("starting server")
+server.listen(8080, '127.0.0.1');
+
+export function read_file_and_send_to_socket(c) {
+  fs.readFile(fileName, function(err, data) {
+    c.write(JSON.stringify({
+      timestamp: new Date(),
+      data: String(data)
+    }))
+    c.end()
+  });
 }
--- test/example.test.mjs
+++ test/example.test.mjs
@@ -1,8 +1,38 @@
 import { expect } from "chai";
-import { sum } from "../src/example.mjs";
+import { read_file_and_send_to_socket, setFileName} from "../src/example.mjs";
+import fs from 'fs'
 
-describe("Example test fixture", () => {
-  it("Example test", () => {
-    expect(sum(1, 2)).to.equal(3);
+describe("Untestable code", () => {
+
+  beforeEach(() => {
+    setFileName("./build/testfile")
+    fs.writeFileSync("./build/testfile", "test-string")
   });
+
+  it("function writes correct information", () => {
+    let data
+      read_file_and_send_to_socket({
+        write: (d) => {
+          data = JSON.parse(d)
+          expect(data.data.includes("test-string"))
+        },
+        end: () => {}
+      })
+  });
+
+
+  it("timestamp is in correct range", () => {
+    const currentTime = new Date();
+    const minuteAgo = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(), currentTime.getHours(), currentTime.getMinutes()-1);
+    const minuteInFuture = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(), currentTime.getHours(), currentTime.getMinutes()+1);
+    let data
+    read_file_and_send_to_socket({
+      write: (d) => {
+        data = JSON.parse(d)
+        expect(minuteAgo < (new Date(data.timestamp)) && (new Date(data.timestamp)) < minuteInFuture).to.be.true
+      },
+      end: () => {}
+    })
+  });
+
 });
